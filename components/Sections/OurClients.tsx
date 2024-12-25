dd0e40a7f6cb91abbd9db9d4583ef60b10ff0723
ismartNishant <nishantrajputall@gmail.com>
2024-12-25 18:17:12 +0530

    images paths fixed now

--- components/Sections/OurClients.tsx
+++ components/Sections/OurClients.tsx
@@ -12,107 +12,107 @@ const Fade = dynamic(() => import('react-awesome-reveal').then((mod) => mod.Fade
 
 const reviews = [
   {
-    img: "/images/ourclients/cr.webp",
+    img: "/Images/ourclients/cr.webp",
   },
   {
-    img: "/images/ourclients/afk.webp",
+    img: "/Images/ourclients/afk.webp",
   },
   {
-    img: "/images/ourclients/riot.webp",
+    img: "/Images/ourclients/riot.webp",
   },
   {
-    img: "/images/ourclients/thefinals.webp",
+    img: "/Images/ourclients/thefinals.webp",
   },
   {
-    img: "/images/ourclients/ampverse.webp",
+    img: "/Images/ourclients/ampverse.webp",
   },
   {
-    img: "/images/ourclients/amazon_pay.webp",
+    img: "/Images/ourclients/amazon_pay.webp",
   },
   {
-    img: "/images/ourclients/apna.webp",
+    img: "/Images/ourclients/apna.webp",
   },
   {
-    img: "/images/ourclients/asaya.webp",
+    img: "/Images/ourclients/asaya.webp",
   },
   {
-    img: "/images/ourclients/castrol.webp",
+    img: "/Images/ourclients/castrol.webp",
   },
   {
-    img: "/images/ourclients/colors.webp",
+    img: "/Images/ourclients/colors.webp",
   },
   {
-    img: "/images/ourclients/doritos.webp",
+    img: "/Images/ourclients/doritos.webp",
   },
   {
-    img: "/images/ourclients/dr_vaidya.webp",
+    img: "/Images/ourclients/dr_vaidya.webp",
   },
   {
-    img: "/images/ourclients/dreamhack.webp",
+    img: "/Images/ourclients/dreamhack.webp",
   },
   {
-    img: "/images/ourclients/eatclub.webp",
+    img: "/Images/ourclients/eatclub.webp",
   },
   {
-    img: "/images/ourclients/grabon.webp",
+    img: "/Images/ourclients/grabon.webp",
   },
   {
-    img: "/images/ourclients/groww.webp",
+    img: "/Images/ourclients/groww.webp",
   },
   {
-    img: "/images/ourclients/harappa.webp",
+    img: "/Images/ourclients/harappa.webp",
   },
   {
-    img: "/images/ourclients/hipi.webp",
+    img: "/Images/ourclients/hipi.webp",
   },
   {
-    img: "/images/ourclients/insider.webp",
+    img: "/Images/ourclients/insider.webp",
   },
   {
-    img: "/images/ourclients/moj.webp",
+    img: "/Images/ourclients/moj.webp",
   },
   {
-    img: "/images/ourclients/mtv.webp",
+    img: "/Images/ourclients/mtv.webp",
   },
   {
-    img: "/images/ourclients/nodwin.webp",
+    img: "/Images/ourclients/nodwin.webp",
   },
   {
-    img: "/images/ourclients/pcf.webp",
+    img: "/Images/ourclients/pcf.webp",
   },
   {
-    img: "/images/ourclients/rapido.webp",
+    img: "/Images/ourclients/rapido.webp",
   },
   {
-    img: "/images/ourclients/sunburn.webp",
+    img: "/Images/ourclients/sunburn.webp",
   },
   {
-    img: "/images/ourclients/swiggy.webp",
+    img: "/Images/ourclients/swiggy.webp",
   },
   {
-    img: "/images/ourclients/tata.webp",
+    img: "/Images/ourclients/tata.webp",
   },
   {
-    img: "/images/ourclients/times_internet.webp",
+    img: "/Images/ourclients/times_internet.webp",
   },
   {
-    img: "/images/ourclients/unsweetened_beauty.webp",
+    img: "/Images/ourclients/unsweetened_beauty.webp",
   },
   {
 
-    img: "/images/ourclients/wakao.webp",
+    img: "/Images/ourclients/wakao.webp",
   },
   {
-    img: "/images/ourclients/weekender.webp",
+    img: "/Images/ourclients/weekender.webp",
   },
   {
-    img: "/images/ourclients/wishcare.webp",
+    img: "/Images/ourclients/wishcare.webp",
   },
   {
-    img: "/images/ourclients/zee.webp",
+    img: "/Images/ourclients/zee.webp",
   },
   {
-    img: "/images/ourclients/zomato.webp",
+    img: "/Images/ourclients/zomato.webp",
   },
 ];
 
